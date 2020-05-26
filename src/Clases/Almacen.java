d854b235608c1524c15a68e2139dee9e8f65a28a
1234 <1234@BANCES>
2020-05-25 20:21:49 -0500

    ...

--- src/Clases/Almacen.java
+++ src/Clases/Almacen.java
@@ -1,45 +1,58 @@
 package Clases;
-//codigo
+
 import java.util.ArrayList;
+
 public class Almacen {
+
     private ArrayList<Productos> listado;
+
     public Almacen() {
         this.listado = new ArrayList<>();
     }
-    public void agregarProducto(int codigo,String nombre,Double precio,int stock,int cod_cat) {
-        Productos c = new Productos(codigo,nombre, precio, stock, cod_cat);
+
+    public void agregarProductos(String nombre, int codigo, String cod_cat, int contenido,
+            String laboratorio, String fecha_prod, String fecha_venc, Double precio, int stock, String fecha_ingreso) {
+        Productos c = new Productos(nombre, codigo, cod_cat, contenido, laboratorio, fecha_prod, fecha_venc, precio, stock, fecha_ingreso);
         this.listado.add(c);
     }
+
     public String buscarProductos(String nombreBuscado) {
         String s = "";
         for (Productos c : this.listado) {
             if (c.getNombre().toUpperCase().contains(nombreBuscado.toUpperCase())) {
-                s += "\nProducto: " + c.getNombre() + "\nPRECIO: " + c.getPrecio();
+                s += "\nProducto: " + c.getNombre() + "\nPRECIO: " + c.getPrecio() + "\nCODIGO: " + c.getCodigo()
+                        + "\nCODIGO DE CATALOGO" + c.getCod_cat() + "\nCONTENIDO: " + c.getContenido() + "\nLABORATORIO: "
+                        + c.getLaboratorio() + "\nFECHA DE PRODUCCIÓN: " + c.getFecha_prod() + "\nFECHA DE VENCIMIENTO: "
+                        + c.getFecha_venc() + "\nSTOCK: " + c.getStock() + "\nFECHA DE INGRESO A ALMACEN: " + c.getFecha_ingreso();
             }
         }
         return s;
     }
+
+    public String buscarProductos(int numero) {
+        numero--;
+        if (numero >= this.listado.size() || numero < 0) {
+            return "Producto no encontrado";
+        }
+        Productos c = this.listado.get(numero);
+        return c.getNombre();
+    }
+
+    public void eliminarProductos(int numero) {
+        numero--;
+        if (numero >= this.listado.size() || numero < 0) {
+            return;
+        }
+
+        this.listado.remove(numero);
+    }
+
     public String obtenerListado() {
         String s = "";
         int idx = 1;
         for (Productos c : this.listado) {
-            s += "\n (" + idx++ + ") " + c.getNombre() +"\nStock: "+c.getStock();
+            s += "\n (" + idx++ + ") " + c.getNombre() + "\nStock: " + c.getStock();
         }
         return s;
     }
-    public String buscarProductos(int numero) {
-        numero--;
-        if (numero >= this.listado.size() || numero < 0) {
-            return "Contacto no encontrado";
-        }
-        Productos c = this.listado.get(numero);
-        return c.getNombre();
-    }public void eliminarProductos(int numero) {
-        numero--;
-        if (numero >= this.listado.size() || numero < 0) 
-            return;
-        
-        this.listado.remove(numero);
-    }
-
 }
